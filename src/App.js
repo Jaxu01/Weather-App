059b795026602efb8da7ab5bd5a7723f4a59830e
Jakub Piwtorak <93015504+Jaxu01@users.noreply.github.com>
2022-03-15 20:40:20 +0100

    Init (#1)
    yes

--- src/App.js
+++ src/App.js
@@ -1,55 +1,50 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
+
 const api = {
-  key: "0860d5dc54a4bd570665697ff1be21e9",
+  key: "dc862ca896c6425009a609e2e15221cb",
   base: "https://api.openweathermap.org/data/2.5/"
 }
 
-function App() {
-  const [query, setQuery] = useState('');
-  const [weather, setWeather] = useState('');
-  const [lat, setLat] = useState([]);
-  const [long, setLong] = useState([]);
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      query: "",
+      weather: {}
+    };
+  }
 
-  useEffect(() => {
-    navigator.geolocation.getCurrentPosition(function(position) {
-      setLat(position.coords.latitude);
-      setLong(position.coords.longitude);
+  componentDidMount() {
+    navigator.geolocation.getCurrentPosition((position) => {
+      const {latitude, longitude} = position.coords;
+      this.lookUpCity(latitude, longitude);
     });
+  }
 
-    console.log("Latitude is:", lat)
-    console.log("Longitude is:", long)
-  }, [lat, long]);
+  async lookUpCity (latitude, longitude) {
+    const result = await fetch(`http://api.openweathermap.org/geo/1.0/reverse?lat=${latitude}&lon=${longitude}&appid=${api.key}`)
+    const resultJSON = await result.json()
 
-  await fetch(`${process.env.REACT_APP_API_URL}/weather/?lat=${lat}&lon=${long}&units=metric&APPID=${process.env.REACT_APP_API_KEY}`)
-  .then(res => res.json())
-  .then(result => {
-    setData(result)
-    console.log(result);
-  });
-}
-fetchData();
-}, [lat,long])
-return (
-  <div className="App">
-    
-  </div>
-);
-}
-  
-  const search = evt => {
+    this.setState({query: resultJSON[0].name})
+    this.lookUpWeather()
+  }
+
+  async lookUpWeather () {
+    const query = this.state.query.replace("gmina ", "")
+    const result = await fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+    const resultJSON = await result.json()
+
+    this.setState({query: "", weather: resultJSON});
+  }
+
+  search (evt) {
     if (evt.key === "Enter") {
-      fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
-        .then(res => res.json())
-        .then(result => {
-          setWeather(result);
-          setQuery('');
-          console.log(result);
-        });
+      this.lookUpWeather()
     }
   }
 
-  const dateBuilder = (d) => {
-    let months = ["Styczeń", "Luty", "Marzec", "Kwiecień", "Maj", "Czerwiec", "Lipiec", "Sierpień", "Wrzesień", "Październik", "Listopad", "Grudzien"];
+  dateBuilder (d) {
+    let months = ["Styczeń", "Luty", "Marzec", "Kwiecień", "Maj", "Czerwiec", "Lipiec", "Sierpień", "Wrzesień", "Październik", "Listopad", "Grudzień"];
     let days = ["Niedziela", "Poniedziałek", "Wtorek", "Środa", "Czwartek", "Piątek", "Sobota"];
 
     let day = days[d.getDay()];
@@ -60,38 +55,40 @@ return (
     return `${day} ${date} ${month} ${year}`
   }
 
-  return (
-    <div className={(typeof weather.main != "undefined")
-     ? ((weather.main.temp > 16) ? 'app warm' : 'app') : 'app'}>
-      <main>
-        <div className="search-box">
-          <input 
-            type="text"
-            className="search-bar"
-            placeholder='Miasto...'
-            onChange={e => setQuery(e.target.value)}
-            value={query}
-            onKeyPress={search}
-          />
-        </div>
-        {(typeof weather.main != "undefined") ? (
-          <div>
-          <div className="location-box">
-              <div className="location">{weather.name}, {weather.sys.country}</div>
-              <div className="date">{dateBuilder(new Date())}</div>
-            </div>
-            <div className="weather-box">
-              <div className="temp">
-                {Math.round(weather.main.temp)}°c
-              </div>
-              <div className="weather">{weather.weather[0].main}</div>
-            </div>
+  render() {
+    return (
+      <div className={(typeof this.state.weather.main != "undefined")
+      ? ((this.state.weather.main.temp > 16) ? 'app warm' : 'app') : 'app'}>
+        <main>
+          <div className="search-box">
+            <input 
+              type="text"
+              className="search-bar"
+              placeholder='Miasto...'
+              onChange={e => this.setState({query:e.target.value})}
+              value={this.query}
+              onKeyPress={this.search.bind(this)}
+            />
           </div>
-        ) : ('')}
-      </main>
-      <footer>Strona stworzona przez Jakuba Piwtoraka</footer>
-    </div>
-  );
+          {(typeof this.state.weather.main != "undefined") ? (
+            <div>
+              <div className="location-box">
+                <div className="location">{this.state.weather.name}, {this.state.weather.sys.country}</div>
+                <div className="date">{this.dateBuilder(new Date())}</div>
+              </div>
+              <div className="weather-box">
+                <div className="temp">
+                  {Math.round(this.state.weather.main.temp)}°c
+                </div>
+                <div className="weather">{this.state.weather.weather[0].main}</div>
+              </div>
+            </div>
+          ) : ''}
+        </main>
+        <footer>Strona stworzona przez Jakuba Piwtoraka</footer>
+      </div>
+    );
+  }
 }
 
 export default App;
